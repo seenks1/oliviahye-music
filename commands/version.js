40a3ebec122799031409ee77740209e094e6e721
Aurii <49456123+seenks1@users.noreply.github.com>
2020-06-10 21:15:46 +0700

    Create version.js

---
+++ commands/version.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args, ops) => {
+    message.reply("**OliviaHye Source v1.3**\nThis bot was never possible without Express source, and you can host your own instance for free on Heroku.\n__Heroku Deploy__ » https://heroku.com/deploy?template=https://github.com/seenks1/oliviahye-music\n__Support Discord server__ » https://discord.gg/7SjFbXx");
+}
